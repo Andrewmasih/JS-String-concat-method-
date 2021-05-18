f8d78392c5116ff6c867e7ff7c60201564d3d75c
Andrew Masih <armasih@outlook.com>
2021-05-18 21:42:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let string1 = "hello";
+let string2 = " there";
+let string3 = " what a beautiful day to learn JavaScript con
+catenation, right?";
+
+console.log (string1.concat (string2, string3));
+
+/* add string2 and string3 to string1 by using the 'concat' method */
